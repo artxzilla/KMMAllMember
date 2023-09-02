d0eaf520da50c0b810d644caee1d7583d7d6da49
Panupong Sathapornnuwong <art@appsynth.net>
2023-09-02 17:01:23 +0700

    add more testing url

--- allshared/src/iosMain/kotlin/co.touchlab/kmmbridgekickstart/SubscriptionInfoInterface.kt
+++ allshared/src/iosMain/kotlin/co.touchlab/kmmbridgekickstart/SubscriptionInfoInterface.kt
@@ -1,6 +1,9 @@
 package co.touchlab.kmmbridgekickstart
 
 interface SubscriptionInfoInterface {
-    val infoUrl: String
+    val subscriptionInfoUrl: String
         get() = "https://uat.cdn.7eleven.io/7now/subscription-th.html"
+
+    val trueWalletManualUrl: String
+        get() = "https://uat.cdn.7eleven.io/7now/true-wallet-manual.html"
 }
